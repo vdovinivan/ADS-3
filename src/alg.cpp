979ae3ce5ea1ed1dce8798d5e3055db1a3a79dab
vdovinivan <99171081+vdovinivan@users.noreply.github.com>
2022-03-06 11:34:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,10 @@
 // Copyright 2021 NNTU-CS
-
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+ int m = 0;
+ for int(j = 0; j <= size; j++) {
+        if (arr[j] == value)
+        m += 1;
+ }
+  return m; 
+ return 0;  
 }
