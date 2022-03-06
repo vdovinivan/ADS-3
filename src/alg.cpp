2d2c3c6ab9e0d891ce649abbaa3d143446fac5a0
vdovinivan <99171081+vdovinivan@users.noreply.github.com>
2022-03-06 11:37:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,8 @@
 // Copyright 2021 NNTU-CS
 int cbinsearch(int *arr, int size, int value) {
  int m = 0;
- for int(j = 0; j <= size; j++) {
-        if (arr[j] == value)
+ for (int j = 0; j <= size; j++) {
+        if (*(arr + j) == value)
         m += 1;
  }
   return m; 
